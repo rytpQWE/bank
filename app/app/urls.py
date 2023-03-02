0de1c9d9bbb1b1859f31bb0fe0928ae9f1141e9d
rytpQWE <rytpiss@gmail.com>
2023-03-02 23:23:52 +0700

    add account api

--- app/app/urls.py
+++ app/app/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from bank.views import AccountView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('django.contrib.auth.urls')),
 ]
+
+router = routers.DefaultRouter()
+router.register('api/account', AccountView)
+urlpatterns += router.urls
