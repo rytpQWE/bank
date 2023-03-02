c2a91b6da3b01d23432b0516d23270717a1727c2
rytpQWE <rytpiss@gmail.com>
2023-03-02 23:24:09 +0700

    add account view

--- app/bank/views.py
+++ app/bank/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
-# Create your views here.
+from bank.models import BankAccount
+from bank.serializers import BankAccountSerializer
+
+
+class AccountView(ReadOnlyModelViewSet):
+    queryset = BankAccount.objects.all()
+    serializer_class = BankAccountSerializer
