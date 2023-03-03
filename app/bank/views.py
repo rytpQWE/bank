ac38fba85f9c845a347c0b45335659c167dd8ae5
rytpQWE <rytpiss@gmail.com>
2023-03-04 00:43:41 +0700

    permis class

--- app/bank/views.py
+++ app/bank/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from bank.models import BankAccount
@@ -6,5 +7,6 @@ from bank.serializers import BankAccountSerializer
 
 
 class AccountView(ReadOnlyModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = BankAccount.objects.all()
     serializer_class = BankAccountSerializer
