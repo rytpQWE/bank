1c824f2b2630769b4f64a2fafeba2dae9218df7b
rytpiQWE <rytpiss@gmail.com>
2023-03-07 20:29:39 +0700

    customer serializer

--- app/bank/serializers.py
+++ app/bank/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from bank.models import BankAccount
+from bank.models import BankAccount, Customer
 
 
 class BankAccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = BankAccount
         fields = ['user', 'balance']
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ['fname, lname', 'city', 'address']
