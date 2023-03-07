4d9e1dc3d27186cbaf02ad99f53bfc5bf2cfb432
rytpiQWE <rytpiss@gmail.com>
2023-03-07 20:30:01 +0700

    CustomerCreateViewSet

--- app/bank/views.py
+++ app/bank/views.py
@@ -1,11 +1,14 @@
+from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
-from bank.models import BankAccount
-from bank.serializers import BankAccountSerializer
+from bank.models import BankAccount, Customer
+from bank.serializers import BankAccountSerializer, CustomerSerializer
 
 
 class AccountViewSet(ReadOnlyModelViewSet):
+    authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
     queryset = BankAccount.objects.all()
     serializer_class = BankAccountSerializer
@@ -13,4 +16,12 @@ class AccountViewSet(ReadOnlyModelViewSet):
     def get_queryset(self):
         """Get object for current user"""
         return self.queryset.filter(user=self.request.user)
+
+
+class CustomerCreateViewSet(generics.RetrieveUpdateAPIView):
+    """GET, PUT, UPDATE """
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
 
