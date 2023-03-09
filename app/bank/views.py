17706353b3f36b3f27e4673cfa4ffff48957668d
rytpiQWE <rytpiss@gmail.com>
2023-03-09 21:22:33 +0700

    delete unused import

--- app/bank/views.py
+++ app/bank/views.py
@@ -1,4 +1,3 @@
-from django.db import transaction
 from rest_framework import generics, viewsets, mixins, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
