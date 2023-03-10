e9a90e0d7faa1d9d6e5c3835578d04211a168c24
rytpiQWE <rytpiss@gmail.com>
2023-03-10 20:06:08 +0700

    register transaction

--- app/bank/admin.py
+++ app/bank/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 
-from bank.models import BankAccount, Customer
+from bank.models import BankAccount, Customer, Transaction
 
 
+@admin.register(Transaction)
 @admin.register(BankAccount)
 @admin.register(Customer)
 class BankAdmin(ModelAdmin):
