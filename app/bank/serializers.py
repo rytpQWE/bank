c148b2bb490d67d0461f701bb77a2e7fe09044ea
rytpiQWE <rytpiss@gmail.com>
2023-03-10 20:05:47 +0700

    add history in serializer

--- app/bank/serializers.py
+++ app/bank/serializers.py
@@ -3,25 +3,29 @@ from rest_framework import serializers
 from bank.models import BankAccount, Customer, Transaction
 
 
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = ['id', 'amount', 'account_from', 'account_to', 'comment', ]
+        read_only_fields = ['id']
+
+
 class BankAccountSerializer(serializers.ModelSerializer):
+    history = TransactionSerializer(many=True, source='account_from')
+
     class Meta:
         model = BankAccount
-        fields = ['user', 'balance']
-        read_only_fields = ['user', 'balance']
+        fields = ['user', 'balance', 'history']
+        read_only_fields = ['user', 'balance', 'history']
 
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = ['fname', 'lname', 'city', 'address']
+
     """Auto add user field(user_id)"""
+
     def create(self, validated_data):
         validated_data['user_id'] = self.context['request'].user
         return super(CustomerSerializer, self).create()
-
-
-class TransactionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Transaction
-        fields = ['id', 'amount', 'account_from', 'account_to', 'comment']
-        read_only_fields = ['id']
